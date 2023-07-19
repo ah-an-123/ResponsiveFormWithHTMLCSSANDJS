63a536a79b9f53f0e68b0edb74773e94fafd8baf
Ahsan <ahsan@gmail.com>
2023-07-19 10:26:29 +0500

    Just Added Some New Logics

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let title=document.getElementById("title")
-let nameField=document.getElementById("nameField")
-let login=document.getElementById("login")
-let signUp=document.getElementById("signUp")
+let title = document.getElementById("title");
+let nameField = document.getElementById("nameField");
+let login = document.getElementById("login");
+let signUp = document.getElementById("signUp");
+let span = document.getElementById("span");
 
-
-signUp.onclick=()=>{
-    title.innerHTML="Sign Up"
-    nameField.hidden=false
-}
-
-login.onclick=()=>{
-    title.innerHTML="Login"
-    nameField.hidden=true
-}
+signUp.onclick = () => {
+  if (signUp.innerHTML == "Sign Up") {
+    title.innerHTML = "Sign Up";
+    login.value = "Sign Up";
+    span.innerHTML = "Have an acoount ?";
+    nameField.hidden = false;
+    signUp.innerHTML = "Login";
+  } else {
+    title.innerHTML = "Login";
+    nameField.hidden = true;
+    login.value = "Login";
+    span.innerHTML = "Not a member ?";
+    signUp.innerHTML = "Sign Up";
+  }
+};
